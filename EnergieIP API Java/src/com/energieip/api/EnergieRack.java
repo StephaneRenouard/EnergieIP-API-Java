52ac0a3a5f4fabeec7442a01cadde83827a10050
stephanerenouard <stephane.renouard@gmail.com>
2017-11-06 17:50:50 +0100

    Add interface add class add classpath

---
+++ EnergieIP API Java/src/com/energieip/api/EnergieRack.java
@@ -0,0 +1,41 @@
+package com.energieip.api;
+
+public class EnergieRack implements EnergieRackInterface {
+
+	@Override
+	public boolean connect(String IP, int port) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean connect(String IP) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean connect() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean disconnect() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void getRackID() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getList() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ EnergieIP API Java/src/com/energieip/api/EnergieRackInterface.java
@@ -0,0 +1,17 @@
+package com.energieip.api;
+
+public interface EnergieRackInterface {
+
+	boolean connect(String IP, int port);
+
+	boolean connect(String IP);
+
+	boolean connect();
+
+	boolean disconnect();
+
+	void getRackID();
+
+	void getList();
+
+}
