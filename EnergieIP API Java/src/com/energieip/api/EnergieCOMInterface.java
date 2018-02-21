d19b5e28c379f8b9e0757e36671e89009ceca1cc
stephanerenouard <stephane.renouard@gmail.com>
2018-02-21 08:42:32 +0100

    add COM interface (communication purpose)

---
+++ EnergieIP API Java/src/com/energieip/api/EnergieCOMInterface.java
@@ -0,0 +1,14 @@
+package com.energieip.api;
+
+public interface EnergieCOMInterface {
+	
+
+	/*
+	 * TCP parameters
+	 */
+	
+	void set_TCPserver_IP(String tcpServer_IP);
+	void set_TCPserver_PORT(int tcpServer_PORT);
+
+
+}
