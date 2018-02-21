8a2b01498acdf4d08d6301fa5f99d12f109270d2
stephanerenouard <stephane.renouard@gmail.com>
2018-02-21 07:54:39 +0100

    update interface with IO (ID14)

--- EnergieIP API Java/src/com/energieip/api/EnergieAPIInterface.java
+++ EnergieIP API Java/src/com/energieip/api/EnergieAPIInterface.java
@@ -246,6 +246,9 @@ public interface EnergieAPIInterface {
 	int get_HVAC_group_calibration_T_for_10V(int group);
 	
 	// GET ID100-PARAMETERS
+	int get_HVAC_parameters_temperature_target(int group); // RW, 100-300, 1/10C, def 220 
+	int get_HVAC_parameters_temperature_offset(int group); // RW, -100/+100, %, def 0 
+	int get_HVAC_parameters_temperature_offset_ladder(int group); // RW, 1-10s, C, def 3 
 	int get_HVAC_parameters_temporisation_comfort_mode(int group); // RW, 0-240, min, def 0
 	int get_HVAC_parameters_temporisation_sleep_mode(int group); // RW, 0-720, min, def 10
     int get_HVAC_parameters_temporisation_reduced_mode(int group); // RW, 0-720, min, def 5
@@ -309,15 +312,91 @@ public interface EnergieAPIInterface {
 	boolean set_HVAC_group_calibration_C0_for_10V(int group, int value);
 	boolean set_HVAC_group_calibration_T_for_10V(int group, int value);
 		
-	
 	// SET ID100-PARAMETERS
+	boolean set_HVAC_parameters_temperature_target(int group, int value); // RW, 100-300, 1/10C, def 220 
+	boolean set_HVAC_parameters_temperature_offset(int group, int value); // RW, -100/+100, %, def 0 
+	boolean set_HVAC_parameters_temperature_offset_ladder(int group, int value); // RW, 1-10s, C, def 3 
+	boolean set_HVAC_parameters_temporisation_comfort_mode(int group, int value); // RW, 0-240, min, def 0
+	boolean set_HVAC_parameters_temporisation_sleep_mode(int group, int value); // RW, 0-720, min, def 10
+    boolean set_HVAC_parameters_temporisation_reduced_mode(int group, int value); // RW, 0-720, min, def 5
+	boolean set_HVAC_parameters_deadband_comfort_mode(int group, int value); // RW, 0-20, �C, def 2
+	boolean set_HVAC_parameters_deadBand_sleep_mode(int group, int value); // RW, 0-20, �C, def 4
+	boolean set_HVAC_parameters_deadBand_reduced_mode(int group, int value); // RW, 0-20, �C, def 6
+	boolean set_HVAC_parameters_proportionalBand_cold(int group, int value); // RW, 2-10, C, def 2
+	boolean set_HVAC_parameters_limit_valve_opening_cold(int group, int value); // RW, 10-100, %, def 100
+	boolean set_HVAC_parameters_proportionalBand_hot(int group, int value); // RW, 2-10, C, def 2
+	boolean set_HVAC_parameters_limit_valve_opening_hot(int group, int value); // RW, 10-100, %, def 100
+	boolean set_HVAC_parameters_fan_proportionalBand(int group, int value); // RW, 2-10, C, def 2
+	boolean set_HVAC_parameters_fan_speed_min(int group, int value); // RW, 0-100; %, def 0
+	boolean set_HVAC_parameters_fan_speed_max(int group, int value); // RW, 0-100; %, def 100
+	boolean set_HVAC_parameters_measure_integral_slope(int group, int value); // RW, 0-100, i, def 10
+	boolean set_HVAC_parameters_measure_slope(int group, int value); // RW, 1-1000, i, def 100
+	boolean set_HVAC_parameters_fan_threshold_low(int group, int value); // RW, 10-50, %, def 20
+	boolean set_HVAC_parameters_fan_threshold_high(int group, int value); // RW, 10-50, %, def 80
+	boolean set_HVAC_parameters_fan_valve_coef(int group, int value); // RW, 1-10000, %, def 100
+	boolean set_HVAC_parameters_airQuality_target(int group, int value); // RW, 0-30000, ppm, def 700
+	boolean set_HVAC_parameters_airQuality_proportionalBand(int group, int value); // RW, 0-30000, ppm, def 800
+	boolean set_HVAC_parameters_VaV_opening_max(int group, int value); // RW, 0-100, %, def 100
+	boolean set_HVAC_parameters_VaV_opening_min(int group, int value); // RW, 0-100, %, def 0
+	boolean set_HVAC_parameters_6Way_tension_100_cold(int group, int value); // RW, 0-100, %, def 5
+	boolean set_HVAC_parameters_6Way_tension_0_cold(int group, int value); // RW, 0-100, %, def 37
+	boolean set_HVAC_parameters_6Way_tension_0_hot(int group, int value); // RW, 0-100, %, def 68
+	boolean set_HVAC_parameters_6Way_tension_100_hot(int group, int value); // RW, 0-100, %, def 100
+	boolean set_HVAC_parameters_dewPoint_tempo(int group, int value); // RW, 0-43200, sec, def 300
 	
 	
 	// ID100 other
+	@Deprecated
 	boolean set_HVAC_temp_target(int group, int value);
 	
 	@Deprecated
 	boolean set_HVAC_auto(int group, int value);
+	
+	
+	/*
+	 *  IO DRIVER
+	 */
+	
+	// GET from ID14
+	int get_IO_input_1(int SA);
+	int get_IO_input_2(int SA);
+	int get_IO_input_3(int SA);
+	int get_IO_input_4(int SA);
+	short get_IO_input_all(int SA);
+	
+	int get_IO_output_1(int SA);
+	int get_IO_output_2(int SA);
+	int get_IO_output_3(int SA);
+	int get_IO_output_4(int SA);
+	int get_IO_output_5(int SA);
+	int get_IO_output_6(int SA);
+	int get_IO_output_7(int SA);
+	int get_IO_output_8(int SA);
+	short get_IO_output_all(int SA);
+	
+	// GET from ID2
+	int get_IO_power(int SA);
+	int get_IO_version(int SA);
+	int get_IO_error(int SA);
+	int get_IO_group(int SA);
+	
+	int get_IO_position_on_switch(int SA);
+	int get_IO_position_on_daughterBoard(int SA);
+	int get_IO_position_on_table(int SA);
+	int get_IO_manufacturing_year(int SA);
+	int get_IO_manufacturing_month(int SA);
+	
+	
+	// SET from ID14
+	boolean set_IO_output_1(int SA, boolean value);
+	boolean set_IO_output_2(int SA, boolean value);
+	boolean set_IO_output_3(int SA, boolean value);
+	boolean set_IO_output_4(int SA, boolean value);
+	boolean set_IO_output_5(int SA, boolean value);
+	boolean set_IO_output_6(int SA, boolean value);
+	boolean set_IO_output_7(int SA, boolean value);
+	boolean set_IO_output_8(int SA, boolean value);
+	boolean set_IO_output_all(int SA, short value);
 	
 	
 	/*
