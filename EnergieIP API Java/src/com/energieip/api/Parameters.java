16ebe84e88715d8933a85092986de037abef59b0
stephanerenouard <stephane.renouard@gmail.com>
2018-02-21 08:58:59 +0100

    change Parameters to public

--- EnergieIP API Java/src/com/energieip/api/Parameters.java
+++ EnergieIP API Java/src/com/energieip/api/Parameters.java
@@ -5,7 +5,7 @@ public class Parameters {
 	/*
 	 * IP params
 	 */
-	static String CORE_IP = "127.0.0.1";
-	static int CORE_PORT = 8082;
+	public static String CORE_IP = "127.0.0.1";
+	public static int CORE_PORT = 8082;
 
 }
