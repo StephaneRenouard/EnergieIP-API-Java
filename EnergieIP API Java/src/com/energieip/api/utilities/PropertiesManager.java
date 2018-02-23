a3fc4be58353457af5026887393c8f71078a197e
StephaneRenouard <stephane.renouard@gmail.com>
2018-02-23 17:24:06 +0100

    update PropertiesManager

--- EnergieIP API Java/src/com/energieip/api/utilities/PropertiesManager.java
+++ EnergieIP API Java/src/com/energieip/api/utilities/PropertiesManager.java
@@ -1,9 +1,11 @@
-package com.energieip.api;
+package com.energieip.api.utilities;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
+
+import com.energieip.api.Parameters;
 
 /**
  * This class gets propoerties from .properties files
