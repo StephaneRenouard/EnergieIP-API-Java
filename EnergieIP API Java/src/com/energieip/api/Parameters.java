ec5f4f876f132ad5367a4c3d788ed36c1bbd21de
stef <stef@Gilgamesh>
2018-09-04 23:53:46 +0200

    update param

--- EnergieIP API Java/src/com/energieip/api/Parameters.java
+++ EnergieIP API Java/src/com/energieip/api/Parameters.java
@@ -5,7 +5,8 @@ public class Parameters {
 	/*
 	 * IP params
 	 */
-	public static String CORE_IP = "127.0.0.1";
+	//public static String CORE_IP = "127.0.0.1";
+	public static String CORE_IP = "192.168.0.25";
 	public static int CORE_PORT = 8082;
 	
 	public static String USERNAME = "test";
